5ab0cdeeaf661e323951fe0566e7ee82961ae25c
josephodom <ejosephodom@gmail.com>
2020-01-29 16:09:08 -0500

    Fixed base tag

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<base href="/resume/tinyrouter/">
+<base href="/tinyrouter/">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="./css/style.css">
 <title>TinyRouter &bull; A tiny PHP router</title>
